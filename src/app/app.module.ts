fb4a303b57a06b9568c3bf685a235af37a19c791
Abidstic <abid71@student.sust.edu>
2023-02-13 15:09:46 +0600

    Added a new componenet called server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { CoursesComponent } from './courses/courses.component';
 import {CourseComponent} from "./course.component";
 import {CourseService} from "./course.service";
 import {SummaryPipe} from "./summary.pipe";
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    SummaryPipe
+    SummaryPipe,
+    ServerComponent
   ],
   imports: [
     BrowserModule
