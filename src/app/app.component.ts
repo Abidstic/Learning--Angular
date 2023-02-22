52f32afebfe62bbefc6642c21b2ec3635b538664
MahinAbid <abid.mahmud@orbitax.com>
2023-02-22 19:44:26 +0600

    added 3 componenet and introduced property and event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Demo';
+  oddNumber:number[]=[];
+  evenNumber:number[]=[];
+
+
+  onIntervalFired(FiredEvent: number) {
+   if(FiredEvent%2==0)
+   {
+     this.evenNumber.push(FiredEvent);
+   }
+   else {
+     this.oddNumber.push(FiredEvent);
+   }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,9 @@ import {SuccessAlertComponent} from "./success-alert/success-alert.component";
 import { ReactiveFormsModule} from "@angular/forms";
 import {HeaderComponent} from "./header/header.component";
 import { RecipeComponent } from './recipe/recipe.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +28,10 @@ import { RecipeComponent } from './recipe/recipe.component';
     WarningAlertComponent,
     SuccessAlertComponent,
     HeaderComponent,
-    RecipeComponent
+    RecipeComponent,
+    GameControlComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule,
